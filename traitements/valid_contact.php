c3b26bf059a84e75bb15782a396bceb6ba093ba7
Charlie-git-hub <charlieemail@proton.me>
2024-06-12 11:10:51 +0200

    mail fonctionnel

--- traitements/valid_contact.php
+++ traitements/valid_contact.php
@@ -1,82 +1,67 @@
 <?php
 session_start();
-$_SESSION['information'] = '';
 
-if (count($_POST) == 0) {
-    header('location: ../form_contact.php');
+if (empty($_POST)) {
+    header('Location: ../form_contact.php');
+    exit();
 }
 
 $affichage_retour = '';
 $erreurs = 0;
 
-if (!empty($_POST['nom'])) {
-    $nom = $_POST['nom'];
-} else {
+if (empty($_POST['nom'])) {
     $affichage_retour .= 'Le champ NOM est obligatoire<br>';
     $erreurs++;
-}
-
-if (!empty($_POST['email'])) {
-    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        $email = $_POST['email'];
-    } else {
-        $affichage_retour .= 'Adresse mail incorrecte<br>';
-        $erreurs++;
-    }
 } else {
-    $affichage_retour .= 'Le champ EMAIL est obligatoire<br>';
+    $nom = $_POST['nom'];
+}
+
+if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $affichage_retour .= 'Adresse e-mail incorrecte<br>';
     $erreurs++;
+} else {
+    $email = $_POST['email'];
 }
 
-if ($erreurs != 0) {
+if ($erreurs > 0) {
     $_SESSION['information'] = $affichage_retour;
-    header('location: ../form_contact.php'); 
+    header('Location: ../form_contact.php');
+    exit();
 }
 
-$prenom = !empty($_POST['prenom']) ? $_POST['prenom'] : ''; 
-$message = !empty($_POST['message']) ? $_POST['message'] : ''; 
-
-echo 'Votre nom : ' . htmlspecialchars($prenom) . ' ' . htmlspecialchars($nom) . '<br>';
-echo 'Adresse mail : ' . htmlspecialchars($email) . '<br>';
-echo 'Message : ' . htmlspecialchars($message) . '<br>';
-
-$prenom = mb_strtolower($prenom);
-$nom = mb_strtolower($nom);
-
+$prenom = !empty($_POST['prenom']) ? $_POST['prenom'] : '';
+$message = !empty($_POST['message']) ? $_POST['message'] : '';
 $subject = 'Potajou : demande de ' . $prenom . ' ' . $nom;
+
 $headers = [
     'From' => $email,
-    'Reply-to' => $email,
+    'Reply-To' => $email,
     'X-Mailer' => 'PHP/' . phpversion()
 ];
+
 $email_dest = "mmi23c09@mmi-troyes.fr";
 
 if (mail($email_dest, $subject, $message, $headers)) {
-    $affichage_retour = 'Mail de Contact OK';
+    $affichage_retour = 'Votre demande a bien été prise en compte. ';
 } else {
-    $affichage_retour = 'Erreur d\'envoi du mail de contact';
+    $affichage_retour = 'Une erreur est survenue lors de l\'envoi de votre demande. Veuillez réessayer plus tard.';
 }
+$subject = 'Confirmation, Potajou';
 
+$headers = [
+    'From' => "mmi23c09@mmi-troyes.fr",
+    'Reply-To' => "mmi23c09@mmi-troyes.fr",
+    'X-Mailer' => 'PHP/' . phpversion()
+];
+
+$email_dest = $email;
+
+if (mail($email_dest, $subject, $message, $headers)) {
+    $affichage_retour = 'Un e-mail de confirmation vous a été envoyé.';
+} else {
+    $affichage_retour = 'Le mail de confiramtion n\'a pas été envoyé. Veuillez réessayer plus tard.';
+}
 $_SESSION['information'] = $affichage_retour;
-header('location: ../form_contact.php'); 
+header('Location: ../form_contact.php');
 exit();
 ?>
-<main>
-<?php
-if ($erreurs == 0) {
-    echo '<div id="reussite">' . "\n";
-    echo '<p>' . htmlspecialchars($_SESSION['information']) . '</p>' . "\n";
-    echo '<form action="../index.php">' . "\n";
-    echo '<button type="submit">Retour</button>' . "\n";
-    echo '</form>' . "\n";
-    echo '</div>' . "\n";
-} else {
-    echo '<div id="echec">' . "\n";
-    echo '<p>' . htmlspecialchars($_SESSION['information']) . '</p>' . "\n";
-    echo '<form action="../form_contact.php">' . "\n";
-    echo '<button type="submit">Retour</button>' . "\n";
-    echo '</form>' . "\n";
-    echo '</div>' . "\n";
-}
-?>
-</main>
